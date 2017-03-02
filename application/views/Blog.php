b79e9b962e4ec0d9c98e77ea0061e02199941eba
rickypratama <ricky.ghifary1122@gmail.com>
2017-03-02 19:56:13 +0700

    add project

---
+++ application/views/Blog.php
@@ -0,0 +1,11 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Blog extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Hello World</h1>";		
+	}
+
+}
